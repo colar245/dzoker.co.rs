66786ad86d73cda4d93ca7dff1241069e3a30c70
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2022-06-26 13:13:18 +0200

    added preloader

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,11 @@
+document.body.onload = function () {
+	setTimeout(function () {
+	  var preloader = document.getElementById("preloader");
+	  if (!preloader.classList.contains("done")) {
+		preloader.classList.add("done");
+	  }
+	}, 1000);
+};
 function toggleMenu(){
     const menuToggle = document.querySelector('.menuToggle');
     const menu = document.querySelector('.menu');
