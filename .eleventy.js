155f332d5e53e2a6920c39ca92ed72c06eb021eb
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2022-07-01 00:39:15 +0200

    added favicon svg; overflov changes; minor eddits..

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (config) {
   config.addPassthroughCopy("src/assets");
   config.addPassthroughCopy("src/sitemap.xml");
   config.addPassthroughCopy("src/robots.txt");
+  config.addPassthroughCopy("src/favicon.svg");
   return {
     dir: {
       input: "src",
